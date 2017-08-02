b1be8920ac9983d09ec619e98703008421480650
Don Bower <don.bower@outlook.com>
2017-08-02 07:59:17 -0700

    new bme example

--- bme280.py
+++ bme280.py
@@ -1,12 +1,17 @@
 from Adafruit_BME280 import *
+import time
 
 sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
 
-degrees = sensor.read_temperature()
-pascals = sensor.read_pressure()
-hectopascals = pascals / 100
-humidity = sensor.read_humidity()
+while True:
 
-print 'Temp      = {0:0.3f} deg C'.format(degrees)
-print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
-print 'Humidity  = {0:0.2f} %'.format(humidity)
+    degrees = sensor.read_temperature()
+    pascals = sensor.read_pressure()
+    hectopascals = pascals / 100
+    humidity = sensor.read_humidity()
+    print '{0:0.3f} deg C {0:0.2f} hPa {0:0.2f} %'.format(degrees, hectopascals, humidity)
+    time.sleep(5)
+
+#    print 'Temp      = {0:0.3f} deg C'.format(degrees)
+#    print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+#    print 'Humidity  = {0:0.2f} %'.format(humidity)
