17734b9f7499b4fb0ecd3a2cb89b00cda5d7a43f
Don <don.bower@outlook.com>
2017-08-04 03:31:37 +0000

    Here is the code

---
+++ TSL2591/readlux.py
@@ -0,0 +1,30 @@
+import tsl2591
+VISIBLE = 2  # channel 0 - channel 1
+INFRARED = 1  # channel 1
+FULLSPECTRUM = 0  # channel 0
+
+GAIN_LOW = 0x00  # low gain (1x)
+GAIN_MED = 0x10  # medium gain (25x)
+GAIN_HIGH = 0x20  # medium gain (428x)
+GAIN_MAX = 0x30  # max gain (9876x)
+
+tsl = tsl2591.Tsl2591()  # initialize
+
+thisGain = tsl.get_gain()
+print('This Gain is {}'.format(thisGain))
+
+tsl.set_gain(GAIN_MED)
+
+thisGain = tsl.get_gain()
+print('New Gain is {}'.format(thisGain))
+
+
+full, ir = tsl.get_full_luminosity()  # read raw values (full spectrum and ir spectrum)
+print full, ir
+lux = tsl.calculate_lux(full, ir)  # convert raw values to lux
+print full, lux, ir
+newfull = tsl.get_luminosity(FULLSPECTRUM)
+newlux = tsl.get_luminosity(VISIBLE)
+newir = tsl.get_luminosity(INFRARED)
+
+print newfull, newlux, newir
