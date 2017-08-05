67843fadef58d8053d9c765a4de385551cb97ed3
Don Bower <don.bower@outlook.com>
2017-08-04 18:48:35 -0700

    better formating

--- bme280.py
+++ bme280.py
@@ -33,9 +33,9 @@ def loop():
             hectopascals = pascals / 100
             TimeStampStr = time.strftime("%Y-%m-%d %H:%M:%S")
             if readcount > 6:
-                print('{0:18}{1:0.3f}C {2:0.2f}hPa {3:0.2f}%'.format(TimeStampStr,degrees, hectopascals, humidity))
+                print('{0:18} {1:0.3f}C {2:0.2f}hPa {3:0.2f}%'.format(TimeStampStr,degrees, hectopascals, humidity))
                 readcount = 0
-            F1.write('{0:18}{1:0.3f}C {2:0.2f}hPa {3:0.2f}%\n'.format(TimeStampStr,degrees, hectopascals, humidity))
+            F1.write('{0:18} {1:0.3f}C {2:0.2f}hPa {3:0.2f}%\n'.format(TimeStampStr,degrees, hectopascals, humidity))
         else:
             print ("Failed to get WX readings, will retry in 5 seconds")
         time.sleep(5)
