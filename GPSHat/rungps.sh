dc0deba7cda1288c2588960cbd9ae260a42555f8
Don Bower <don.bower@outlook.com>
2017-08-05 00:49:22 -0700

    reformat print string

--- GPSHat/rungps.sh
+++ GPSHat/rungps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+IFS=":"
 BASEDIR="/mnt/usbstick/data"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 LOGFILE=$BASEDIR/gpslog.txt
@@ -6,6 +7,25 @@ RAWFILE=$BASEDIR/rawdata/gpsraw-$DATE.txt
 DATAFILE=$BASEDIR/gpsdata-$DATE.txt
 ts=$(date +"%Y/%m/%d %H:%M:%S.%N")
 echo $ts "Start GPS Process" >> $LOGFILE
+
+function extract_dms()
+{
+  degrees_mf=`echo $gpgga_pos | cut -d '.' -f 1`
+  if [[ ${#degrees_mf} -eq 3 ]]; then
+    degrees=${1:0:3}
+    minutes=${1:3:2}
+  else
+    degrees=${1:0:2}
+    minutes=${1:2:2}
+  fi
+  seconds=`echo $gpgga_pos | cut -d '.' -f 2`
+  printf ("%3d:%2d:%4d",degrees, minutes, seconds)
+}
+
+function dms_to_dd(degrees, minutes, seconds) {
+  dd=`echo $degrees $minutes $seconds | awk '{printf ("%.4f\n",$1+$2/60+$3/3600)}'`
+  printf ("%3.6f",dd)
+}
 while [ 1 ]
   do
       read this_line
@@ -37,6 +57,12 @@ while [ 1 ]
     		gps_elev=$(echo $this_line | cut -d, -f 10)
   #  		echo $this_line >> $DATAFILE
 #    		sleep 5s
+        extract_dms(gps_latdeg) | read lat_d lat_m lat_s
+        extract_dms(gps_londeg) | read lon_d lon_m lon_s
+        dms_to_dd(lat_d, lat_m, lat_s) | read lat_dd
+        dms_to_dd(lon_d, lon_m, lon_s) | read lon_dd
+        printf("%22s %3.6f %3.6f",$ts,lat_dd, lon_dd)
+        printf("%22s %3.6f %3.6f",$ts,lat_dd, lon_dd) >> %DATAFILE
   	fi
 
   	if [[ $this_line == \$GPRMC* ]]
@@ -54,7 +80,7 @@ while [ 1 ]
               # show
   			if [[ "$gps_fix" != "V" ]]
   			then
-  				echo -e "$ts ${gps_pos[@]}" >> $DATAFILE
+#  				echo -e "$ts ${gps_pos[@]}" >> $DATAFILE
   				sleep 5s
   			fi
   		fi
