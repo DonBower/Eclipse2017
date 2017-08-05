d70ea1d74182275e02ff24330385fa952b28e828
Don Bower <don.bower@outlook.com>
2017-08-04 18:19:02 -0700

    Create Loging, Screen output

--- bme280.py
+++ bme280.py
@@ -1,18 +1,48 @@
 #!/usr/bin/env python3
+'''
+  runbme.py
+  Collect Temperature, Humidity & Barometric Pressure and Temperature from BME280 sensor
+  and record them into a log file
+  2017-08-04 - Donald Bower
+'''
 from Adafruit_BME280 import *
 import time
 
-sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+BASEDIR = "/mnt/usbstick/data"
+DATESTR = time.strftime("%Y-%m-%d_%H:%M:%S")
+DATAFILE='$BASEDIR/wxdata_{:s}.txt'.format(DATESTR)
 
-while True:
+F1 = open(DATAFILE, "w", 1) # Open File, write to disk every line.
 
-    degrees = sensor.read_temperature()
-    pascals = sensor.read_pressure()
-    hectopascals = pascals / 100
-    humidity = sensor.read_humidity()
-    print('{0:0.3f} deg C {0:0.2f} hPa {0:0.2f} %'.format(degrees, hectopascals, humidity))
-    time.sleep(5)
+def setup():
+	print ("Setting up BME280 Sensor, please wait...")
+	print "Open File ", DATAFILE, " for append"
 
-#    print 'Temp      = {0:0.3f} deg C'.format(degrees)
-#    print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
-#    print 'Humidity  = {0:0.2f} %'.format(humidity)
+def loop():
+	sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+
+	while True:
+        degrees = sensor.read_temperature()
+        pascals = sensor.read_pressure()
+        humidity = sensor.read_humidity()
+        readcount = readcount + 1
+		if degrees is not None and pascals is not None and humidity is not None:
+            hectopascals = pascals / 100
+			TimeStampStr = time.strftime("%Y-%m-%d %H:%M:%S")
+            if readcount > 6:
+                print('{0:18}{1:0.3f}C {2:0.2f}hPa {3:0.2f}%'.format(TimeStampStr,degrees, hectopascals, humidity))
+                readcount = 0
+            F1.write('{0:18}{1:0.3f}C {2:0.2f}hPa {3:0.2f}%'.format(TimeStampStr,degrees, hectopascals, humidity))
+		else:
+			print ("Failed to get WX readings, will retry in 5 seconds")
+		time.sleep(5)
+
+def destroy():
+	F1.close()
+
+if __name__ == "__main__":
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:
+		destroy()
