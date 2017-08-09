b476b332557593b170e5313a740520f7926d619a
Don Bower <don.bower@outlook.com>
2017-08-08 22:26:08 -0700

    this should work...

--- GPSHat/rungps.sh
+++ GPSHat/rungps.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
+inputDevice=/dev/ttyAMA0
+#inputDevice=/Users/don/Developer/Eclipse2017/gpststdta.txt
 IFS=":"
 BASEDIR="/mnt/usbstick/data"
+BASEDIR="/Users/don/Developer/Eclipse2017"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 LOGFILE=$BASEDIR/gpslog.txt
 RAWFILE=$BASEDIR/rawdata/gpsraw-$DATE.txt
 DATAFILE=$BASEDIR/gpsdata-$DATE.txt
+
 ts=$(date +"%Y/%m/%d %H:%M:%S.%N")
 echo $ts "Start GPS Process" >> $LOGFILE
 
-function extract_dms()
-{
-  echo -e "convert $1 to dms"
-  degrees_mf=`echo $1 | cut -d '.' -f 1`
-  if [[ ${#degrees_mf} -eq 5 ]]; then
-    echo "degrees is 5 in length"
-    degrees=${1:0:3}
-    minutes=${1:3:2}
-    echo -e "degrees is $degrees"
-    echo -e "munutes is $minutes"
-  else
-    echo "degrees is not 5 in length"
-    degrees=${1:0:2}
-    minutes=${1:2:2}
-    echo -e "degrees is $degrees"
-    echo -e "munutes is $minutes"
-  fi
-  seconds=`echo $1 | cut -d '.' -f 2`
-  printf "%i:%i:%i\n" $degrees $minutes $seconds
+echo -e "inputDevice is $inputDevice"
+
+function convert() {
+degrees_mf=`echo $1 | cut -d '.' -f 1`
+if [[ ${#degrees_mf} -eq 5 ]]; then
+  deg=${1:0:3}
+  min=${1:3:2}
+  s=${1:6:4}
+
+else
+  deg=${1:0:2}
+  min=${1:2:2}
+  s=${1:5:4}
+fi
+sec=$(echo "scale=4; $s/10000 * 60" | bc)
 }
 
 function dms_to_dd() {
-  dd=`echo $1 $2 $3 | awk '{printf ("%.4f\n",$1+$2/60+$3/3600)}'`
-  printf "%3.6f\n" $dd
+  dd=`echo "scale=6; $1 + $2/60 + $3/3600" | bc`
 }
 
-while [ 1 ]
+while [ True ]
   do
       read this_line
 
@@ -42,7 +41,6 @@ while [ 1 ]
       # (os-specfic concern)
       #
       this_line=$( echo $this_line | sed s/$'\r'//g )
-
       # get a precise time stamp
       # %N = nanoseconds
       #
@@ -63,16 +61,26 @@ while [ 1 ]
     		gps_sats=$(echo $this_line | cut -d, -f 8)
     		gps_hdop=$(echo $this_line | cut -d, -f 9)
     		gps_elev=$(echo $this_line | cut -d, -f 10)
-  #  		echo $this_line >> $DATAFILE
-#    		sleep 5s
+
         printf "gps_latdeg is %5.4f\n" $gps_latdeg
-        extract_dms $gps_latdeg | read lat_d lat_m lat_s
-        printf "extract_dms returned  %i %i %i\n" $lat_d $lat_m $lat_s
-        extract_dms $gps_londeg | read lon_d lon_m lon_s
-        dms_to_dd $lat_d, $lat_m, $lat_s | read lat_dd
-        dms_to_dd $lon_d, $lon_m, $lon_s | read lon_dd
-        printf "%22s %3.6f %3.6f\n" $ts $lat_dd $lon_dd
-        printf "%22s %3.6f %3.6f\n" $ts $lat_dd $lon_dd >> %DATAFILE
+        extract_dms $gps_latdeg
+        printf "extract_dms returned %03i° %02i\' %02.2f\"\n" $deg $min $sec
+        lat_d=$deg
+        lat_m=$min
+        lat_s=$sec
+        dms_to_dd $lat_d $lat_m $lat_s
+        lat_dd = $dd
+        extract_dms $gps_londeg
+        printf "extract_dms returned %03i° %02i\' %02.2f\"\n" $deg $min $sec
+        lon_d=$deg
+        lon_m=$min
+        lon_s=$sec
+        dms_to_dd $lon_d $lon_m $lon_s
+        lat_dd = $dd
+
+        printf "%22s %2.6f %3.6f\n" $ts $lat_dd $lon_dd
+#        printf "%22s %2.6f %3.6f\n" $ts $lat_dd $lon_dd >> %DATAFILE
+        exit
   	fi
 
   	if [[ $this_line == \$GPRMC* ]]
@@ -96,4 +104,4 @@ while [ 1 ]
   		fi
   	fi
 
-done < /dev/ttyAMA0
+done < $inputDevice
