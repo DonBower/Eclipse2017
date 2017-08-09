f281b79e2348a0f891a6ced807724a902fc4b927
Don Bower <don.bower@outlook.com>
2017-08-08 23:01:42 -0700

    more..

--- GPSHat/extract_dms.sh
+++ GPSHat/extract_dms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 this_line=$GPGGA,050241.000,3401.8807,N,11725.0211,W,2,09,1.21,329.8,M,-32.8,M,0000,0000*53
 IFS=":"
-function convert() {
+function extract_dms() {
 degrees_mf=`echo $1 | cut -d '.' -f 1`
 if [[ ${#degrees_mf} -eq 5 ]]; then
   deg=${1:0:3}
