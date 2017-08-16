f8eac79fcc7e445192183c90fc9481c150b4fbeb
Don Bower <don.bower@outlook.com>
2017-08-15 20:28:07 -0700

    add timestamp

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ GPSERRFILE=$BASEDIR/gpserrlog$DATE.txt
 WXERRFILE=$BASEDIR/wxerrlog$DATE.txt
 LUXERRFILE=$BASEDIR/luxerrlog$DATE.txt
 CAMERRFILE=$BASEDIR/camerrlog$DATE.txt
-echo $ts "Start WX, GPS, CAM" >>  $BASEDIR/startlog.txt
+echo -e "$DATE Start WX, GPS, CAM" >>  $BASEDIR/startlog.txt
 nohup /home/pi/Developer/Eclipse2017/GPSHat/rungps.sh > /dev/null 2>$GPSERRFILE &
 nohup /home/pi/Developer/Eclipse2017/runwx.py > /dev/null 2>$WXERRFILE &
 nohup /home/pi/Developer/Eclipse2017/TSL2591/runlux.py > /dev/null 2>$LUXERRFILE &
