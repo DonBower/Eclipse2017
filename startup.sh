ceb6e3391605a8e9ccdcfcd9e85ad503cd7753a9
Don Bower <don.bower@outlook.com>
2017-08-15 22:40:34 -0700

    remove sudo

--- startup.sh
+++ startup.sh
@@ -5,8 +5,8 @@ GPSERRFILE=$BASEDIR/gpserrlog$DATE.txt
 WXERRFILE=$BASEDIR/wxerrlog$DATE.txt
 LUXERRFILE=$BASEDIR/luxerrlog$DATE.txt
 CAMERRFILE=$BASEDIR/camerrlog$DATE.txt
-sudo -u pi echo -e "$DATE Start WX, GPS, CAM" >>  $BASEDIR/startlog.txt
-sudo -u pi nohup /home/pi/Developer/Eclipse2017/GPSHat/rungps.sh > /dev/null 2>$GPSERRFILE &
-sudo -u pi nohup /home/pi/Developer/Eclipse2017/runwx.py > /dev/null 2>$WXERRFILE &
-sudo -u pi nohup /home/pi/Developer/Eclipse2017/TSL2591/runlux.py > /dev/null 2>$LUXERRFILE &
-sudo -u pi nohup /home/pi/Developer/Eclipse2017/CAMERA/runcam.sh > /dev/null 2>$CAMERRFILE &
+echo -e "$DATE Start WX, GPS, CAM" >>  $BASEDIR/startlog.txt
+nohup /home/pi/Developer/Eclipse2017/GPSHat/rungps.sh > /dev/null 2>$GPSERRFILE &
+nohup /home/pi/Developer/Eclipse2017/runwx.py > /dev/null 2>$WXERRFILE &
+nohup /home/pi/Developer/Eclipse2017/TSL2591/runlux.py > /dev/null 2>$LUXERRFILE &
+nohup /home/pi/Developer/Eclipse2017/CAMERA/runcam.sh > /dev/null 2>$CAMERRFILE &
