38d300a23af63b1db99c078ea614bd44cb5add6b
Don Bower <don.bower@outlook.com>
2017-08-15 22:16:45 -0700

    add second check on string

--- GPSHat/rungps.sh
+++ GPSHat/rungps.sh
@@ -48,10 +48,12 @@ while [ True ]
       timeStamp=$(date +"%H:%M:%S.%N")
 
       echo $timeStamp $this_line >> $RAWFILE
+      gps_chksum=$(echo $this_line | cut -d, -f 15)
+      chksum_star=$(echo $gps_chksum | cut -c1)
 
       # let us filter the current position
       #
-  	if [[ $this_line == \$GPGGA* ]]
+  	if [[ $this_line == \$GPGGA* ] && [ $chksum_star == "*" ]]
     	then
         gps_time=$(echo $this_line | cut -d, -f 2)
         gps_latdeg=$(echo $this_line | cut -d, -f 3)
