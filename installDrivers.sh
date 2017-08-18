40b1fa42fe0391cfefb992853f7d33ccb0f896d7
Don Bower <don.bower@outlook.com>
2017-08-17 20:07:31 -0700

    Initial commit

---
+++ installDrivers.sh
@@ -0,0 +1,15 @@
+cd ~/Developer
+driver=Adafruit_Python_ADS1x15
+
+if [ -d $driver ]
+  then
+    cd $driver
+    git pull
+    cd ~/Developer
+  else
+    git clone git@github.com:adafruit/$driver.git
+fi
+
+cd $driver
+sudo python setup.py install
+sudo python3 setup.py install
