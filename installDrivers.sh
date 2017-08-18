2543ef85cb978d101f3434df192ac81714e1f6f2
Don Bower <don.bower@outlook.com>
2017-08-17 20:20:52 -0700

    drivers via list

--- installDrivers.sh
+++ installDrivers.sh
@@ -1,15 +1,19 @@
-cd ~/Developer
-driver=Adafruit_Python_ADS1x15
-
-if [ -d $driver ]
-  then
-    cd $driver
-    git pull
+driverlist=~/Developer/Eclipse2017/DriverList.txt
+while read driver
+  do
     cd ~/Developer
-  else
-    git clone git@github.com:adafruit/$driver.git
-fi
+    driver=Adafruit_Python_ADS1x15
 
-cd $driver
-sudo python setup.py install
-sudo python3 setup.py install
+    if [ -d $driver ]
+      then
+        cd $driver
+        git pull
+        cd ~/Developer
+      else
+        git clone git@github.com:adafruit/$driver.git
+    fi
+
+    cd $driver
+    sudo python setup.py install
+    sudo python3 setup.py install
+done < $driverlist
