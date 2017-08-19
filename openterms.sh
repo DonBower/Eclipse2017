8a2b20376cbb8d97113c3c44c59d476c3abf1afb
Don Bower <don.bower@outlook.com>
2017-08-19 14:02:03 -0700

    forgot to comment out the nohups.

--- openterms.sh
+++ openterms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
-nohup lxterminal --title="Watch GPS Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchgps.sh" &
-nohup lxterminal --title="Watch LUX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchlux.sh" &
-nohup lxterminal --title="Watch WX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchwx.sh" &
+#nohup lxterminal --title="Watch GPS Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchgps.sh" &
+#nohup lxterminal --title="Watch LUX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchlux.sh" &
+#nohup lxterminal --title="Watch WX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchwx.sh" &
 
 lxterminal --title="Watch GPS Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchgps.sh"
 lxterminal --title="Watch LUX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchlux.sh"
