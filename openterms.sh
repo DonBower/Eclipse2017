e3a99eae6fd0de319286ec75c6e8f5c05926eb4e
Don Bower <don.bower@outlook.com>
2017-08-19 14:07:17 -0700

    set geometry

--- openterms.sh
+++ openterms.sh
@@ -3,6 +3,6 @@
 #nohup lxterminal --title="Watch LUX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchlux.sh" &
 #nohup lxterminal --title="Watch WX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchwx.sh" &
 
-lxterminal --title="Watch GPS Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchgps.sh"
-lxterminal --title="Watch LUX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchlux.sh"
-lxterminal --title="Watch WX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchwx.sh"
+lxterminal --title="Watch GPS Log" --geometry=80x20 --command "/home/pi/Developer/Eclipse2017/watchgps.sh"
+lxterminal --title="Watch LUX Log" --geometry=80x20 --command "/home/pi/Developer/Eclipse2017/watchlux.sh"
+lxterminal --title="Watch WX Log" --geometry=80x20 --command "/home/pi/Developer/Eclipse2017/watchwx.sh"
