7f0a7ededc74f0efff22c477f30a670d7cd11081
Don <Don.Bower@outlook.com>
2017-08-19 10:28:46 -0700

    add preview

--- CAMERA/runcam.sh
+++ CAMERA/runcam.sh
@@ -2,6 +2,7 @@
 BASEDIR="/mnt/usbstick/pictures"
 while true; do 
 	DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-	raspistill -n -vf -hf -o $BASEDIR/$DATE.jpg
-	sleep 5s
+	#raspistill -n -vf -hf -o $BASEDIR/$DATE.jpg
+	raspistill --preview 740,40,540,320 -t 5000 -vf -hf -o $BASEDIR/$DATE.jpg
+	#sleep 5s
 done
