8e2bedd341557974cc4c7981a1a8b51c00caa4f0
Don Bower <don.bower@outlook.com>
2017-08-19 10:33:26 -0700

    add annotation

--- CAMERA/runcam.sh
+++ CAMERA/runcam.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BASEDIR="/mnt/usbstick/pictures"
-while true; do 
+while true; do
 	DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 	#raspistill -n -vf -hf -o $BASEDIR/$DATE.jpg
-	raspistill --preview 740,40,540,320 -t 5000 -vf -hf -o $BASEDIR/$DATE.jpg
+	raspistill --annotate 12 --annotateex 32,0xff,0x808000 --preview 740,40,540,320 --timeout 5000 --vflip --hflip --output $BASEDIR/$DATE.jpg
 	#sleep 5s
 done
