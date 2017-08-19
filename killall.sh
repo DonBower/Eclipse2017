2fc76861e73a5d2ba750fdc3b6cd0d820b228371
Don Bower <don.bower@outlook.com>
2017-08-19 12:19:18 -0700

    don't list myself.

--- killall.sh
+++ killall.sh
@@ -4,6 +4,6 @@ DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
 echo -e "$DATE Reset Data" >> $BASEDIR/startlog.txt
 echo "UID        PID  PPID  C STIME TTY          TIME CMD" >> $BASEDIR/startlog.txt
-ps -ef | grep Eclipse2017 | grep -v grep >> $BASEDIR/startlog.txt
+ps -ef | grep Eclipse2017 | grep -v grep | grep -v killall.sh >> $BASEDIR/startlog.txt
 
 pkill --signal 9 -f '/home/pi/Developer/Eclipse2017'
