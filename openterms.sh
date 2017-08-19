ff35695059ddca41d36d0b14779ab05d902dbf80
Don Bower <don.bower@outlook.com>
2017-08-19 13:59:59 -0700

    closer?

--- openterms.sh
+++ openterms.sh
@@ -2,3 +2,7 @@
 nohup lxterminal --title="Watch GPS Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchgps.sh" &
 nohup lxterminal --title="Watch LUX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchlux.sh" &
 nohup lxterminal --title="Watch WX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchwx.sh" &
+
+lxterminal --title="Watch GPS Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchgps.sh"
+lxterminal --title="Watch LUX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchlux.sh"
+lxterminal --title="Watch WX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchwx.sh"
