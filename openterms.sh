53aa314867eaf3f357ffcb386220ea24a0d149c8
Don Bower <don.bower@outlook.com>
2017-08-19 13:32:28 -0700

    open all three

--- openterms.sh
+++ openterms.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
-BASEDIR=/mnt/usbstick
-gpsfile=`ls -1t $BASEDIR/data/gpsdata* | tail -1`
-
 nohup lxterminal --title="Watch GPS Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchgps.sh" &
+nohup lxterminal --title="Watch LUX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchlux.sh" &
+nohup lxterminal --title="Watch WX Log" -e "bash -c /home/pi/Developer/Eclipse2017/watchwx.sh" &
